f24e92332812ccf00136d34f5a2be9803ed3bda6
Corentin Cadiou <corentin.cadiou@cphyc.me>
2025-07-01 19:35:59 +0200

    Implement star formation

--- core/src/particles/ParticleUpdate.h
+++ core/src/particles/ParticleUpdate.h
@@ -1,11 +1,31 @@
 #pragma once
 
 #include "ParticleUpdate_base.h"
+
+namespace dyablo {
+
+
+class ParticleUpdate_tracers_move;
+class ParticleUpdate_NGP_move;
+class ParticleUpdate_NGP_density;
+class ParticleUpdate_CIC_move;
+class ParticleUpdate_CIC_density;
+class ParticleUpdate_star_formation;
+
+} //namespace dyablo 
+
 #include "plugins_lib.h"
 
 template<>
 inline bool dyablo::ParticleUpdateFactory::init()
 {
   dyablo::load_dyablo_plugins_lib();
+  DECLARE_REGISTERED(dyablo::ParticleUpdate_tracers_move);
+  DECLARE_REGISTERED(dyablo::ParticleUpdate_NGP_move);
+  DECLARE_REGISTERED(dyablo::ParticleUpdate_NGP_density);
+  DECLARE_REGISTERED(dyablo::ParticleUpdate_CIC_move);
+  DECLARE_REGISTERED(dyablo::ParticleUpdate_CIC_density);
+  DECLARE_REGISTERED(dyablo::ParticleUpdate_star_formation);
+
   return true;
 }
