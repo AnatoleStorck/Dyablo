262425aa926743a326d28e9dc9e76812662d8595
Corentin Cadiou <corentin.cadiou@cphyc.me>
2025-09-26 17:25:07 +0200

    Add cooling using Grackle tables

--- core/src/source_terms/SourceUpdate.h
+++ core/src/source_terms/SourceUpdate.h
@@ -2,9 +2,26 @@
 
 #include "source_terms/SourceUpdate_base.h"
 
+namespace dyablo {
+
+class SourceUpdate_Cooling_FF;
+class SourceUpdate_GLM;
+class SourceUpdate_Photons_Beam;
+
+class SourceUpdate_cooling_grackle_table;
+
+} //namespace dyablo 
+
+
 template<>
 inline bool dyablo::SourceUpdateFactory::init()
 {
+  DECLARE_REGISTERED(dyablo::SourceUpdate_Cooling_FF);
+  DECLARE_REGISTERED(dyablo::SourceUpdate_GLM);
+  DECLARE_REGISTERED(dyablo::SourceUpdate_Photons_Beam);
+
+  DECLARE_REGISTERED(dyablo::SourceUpdate_cooling_grackle_table);
+
   dyablo::load_dyablo_plugins_lib();
   return true;
 }
