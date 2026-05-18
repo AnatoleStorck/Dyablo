4df4bbf20ac8615ffcafc033354e2cbd88ebb90e
Anatole Storck <storckanatole@gmail.com>
2026-05-18 16:38:57 -0500

    no metallicity evolution yet, take constant metallicity value for SNII determination

--- core/src/particles/ParticleUpdate_feedback.cpp
+++ core/src/particles/ParticleUpdate_feedback.cpp
@@ -64,9 +64,9 @@ public:
     eta_SNII        ( configMap.getValue<real_t>("star_feedback", "eta_SNII", 0.1) ),
     yield_SNII      ( configMap.getValue<real_t>("star_feedback", "yield_SNII", 0.1) ),
     E_SNII_physical ( configMap.getValue_in_code_unit<Units::Energy>("star_feedback", "E_SNII", "1e51 erg") ),
-    M_SNII_physical ( configMap.getValue_in_code_unit<Units::Mass>  ("star_feedback", "M_SNII", "10 solar_mass") ),
-    t_SNII_physical ( configMap.getValue_in_code_unit<Units::Time>  ("star_feedback", "t_SNII", "10 Myr") ),
-    cosmology       ( configMap.getValue<bool>("cosmology", "active", false) )
+    cosmology       ( configMap.getValue<bool>("cosmology", "active", false) ),
+    // meant to be temporary until we have proper metallicity evolution and SNII yields
+    metallicity_uniform     ( configMap.getValue<real_t>("constant_metallicity", "metallicity") )
   {
   }
 
@@ -92,11 +92,11 @@ public:
     std::vector<UserData::ParticleAccessor_AttributeInfo>
       pinfos = {{"mass", IMASS}, {"birth_mass", IBIRTHMASS}, {"vx", IVX}, {"vy", IVY}, {"vz", IVZ}, {"birth_time", IBIRTH}};
 
-    bool has_metallicity = U.has_field("metallicity");
-    if (has_metallicity) {
-      Uin_infos.push_back( {"metallicity", IRho_Z} );
-      pinfos.push_back( {"metallicity", IMETAL} );
-    }
+    // bool has_metallicity = U.has_field("metallicity");
+    // if (has_metallicity) {
+    //   Uin_infos.push_back( {"metallicity", IRho_Z} );
+    //   pinfos.push_back( {"metallicity", IMETAL} );
+    // }
 
     // Get accessors
     auto Ppos = U.getParticleArray( "particles" );
@@ -132,7 +132,7 @@ public:
       // get mass of stars that leave MS in current time step based on
       // Raiteri et al. 1996 and assuming a Salpeter IMF.
       // Check if M1-M2 mass range is within CCSN progenitor mass range (8-40 solar masses)
-      real_t temp_metallicity = 0.02;
+      real_t temp_metallicity = metallicity_uniform;
 
       real_t M1 = timedelay_SNII::raiteri_ms_mass(age_physical, temp_metallicity);
       real_t M2 = timedelay_SNII::raiteri_ms_mass(age_physical + dt_physical, temp_metallicity);
@@ -183,10 +183,10 @@ public:
         Kokkos::atomic_add(&Uin.at(iCell, IRho_vx), rho_loss * part_vel[IX]);
         Kokkos::atomic_add(&Uin.at(iCell, IRho_vy), rho_loss * part_vel[IY]);
         Kokkos::atomic_add(&Uin.at(iCell, IRho_vz), rho_loss * part_vel[IZ]);
-        if (has_metallicity) {
-          real_t Z_loss = yield_SNII + (1 - yield_SNII) * Pdata.at(iPart, IMETAL);
-          Kokkos::atomic_add(&Uin.at(iCell, IRho_Z), rho_loss * Z_loss);
-        }
+        // if (has_metallicity) {
+        //   real_t Z_loss = yield_SNII + (1 - yield_SNII) * Pdata.at(iPart, IMETAL);
+        //   Kokkos::atomic_add(&Uin.at(iCell, IRho_Z), rho_loss * Z_loss);
+        // }
 
         // Update particle properties
         Pdata.at(iPart, IMASS) -= Mloss;
@@ -249,8 +249,8 @@ private:
   ForeachParticle foreach_particle;
   Timers& timers;
 
-  real_t eta_SNII;
-  real_t yield_SNII;
+  real_t metallicity_uniform;
+
   real_t E_SNII_physical;
   real_t M_SNII_physical;
   real_t t_SNII_physical;
