0fd8d1931e9238de7eaf2cb903e536e393b7fb62
Anatole Storck <storckanatole@gmail.com>
2026-06-12 12:42:22 -0500

    add a 7 passive scalar class for H/He only tests

--- core/src/hyperbolic/HydroUpdate_euler.cpp
+++ core/src/hyperbolic/HydroUpdate_euler.cpp
@@ -15,6 +15,7 @@ public:
 namespace {
   using HyperbolicPolicy_Hydro_2_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_Hydro_impl, 2> >;
   using HyperbolicPolicy_Hydro_4_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_Hydro_impl, 4> >;
+  using HyperbolicPolicy_Hydro_7_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_Hydro_impl, 7> >;
   using HyperbolicPolicy_Hydro_16_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_Hydro_impl, 16> >;
   using HyperbolicPolicy_Hydro_25_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_Hydro_impl, 25> >;
 }
@@ -33,6 +34,14 @@ class HydroUpdate_euler_4_passive_scalars
   
 public:
   using Hyperbolic_euler<HyperbolicPolicy_Hydro_4_passive_scalars>::Hyperbolic_euler;
+};
+
+class HydroUpdate_euler_7_passive_scalars 
+  : public Hyperbolic_euler<HyperbolicPolicy_Hydro_7_passive_scalars>
+{
+  
+public:
+  using Hyperbolic_euler<HyperbolicPolicy_Hydro_7_passive_scalars>::Hyperbolic_euler;
 };
 
 class HydroUpdate_euler_16_passive_scalars 
@@ -64,6 +73,10 @@ FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory,
 FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory, 
                   dyablo::HydroUpdate_euler_4_passive_scalars, 
                   "HydroUpdate_euler_4_passive_scalars")
+
+FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory, 
+                  dyablo::HydroUpdate_euler_7_passive_scalars, 
+                  "HydroUpdate_euler_7_passive_scalars")
 
 FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory, 
                   dyablo::HydroUpdate_euler_16_passive_scalars, 
