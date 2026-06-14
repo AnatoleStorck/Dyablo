e6bb572b5151d029fd52e2c36a34401908f99425
Anatole Storck <storckanatole@gmail.com>
2026-06-14 12:55:40 -0500

    Add option to not use rt timestep if no radiation is currently present

--- core/src/compute_dt/Compute_dt_rad.cpp
+++ core/src/compute_dt/Compute_dt_rad.cpp
@@ -12,7 +12,9 @@ public:
                         ForeachCell& foreach_cell,
                         Timers& timers )
   : foreach_cell(foreach_cell),
-    c_rad( configMap.getValue_in_code_unit<Units::Velocity>("rad", "c_rad", "speedoflight") )
+    c_rad( configMap.getValue_in_code_unit<Units::Velocity>("rad", "c_rad", "speedoflight") ),
+    n_groups( configMap.getValue<int>("rad", "n_groups", 1) ),
+    wait_for_radiation(configMap.getValue<bool>("rad", "wait_for_radiation", false))
   {
     real_t default_cfl = 0.5;
     if (configMap.hasValue("hydro", "cfl")) {
@@ -26,8 +28,47 @@ public:
   {
     real_t aexp = scalar_data.get<real_t>("aexp");
     real_t ctilde = Units::physical_to_supercomoving<Units::Velocity>(this->c_rad , aexp);
+    int n_groups = this->n_groups;
+    bool wait_for_radiation = this->wait_for_radiation;
 
     auto cells = foreach_cell.getCellMetaData();
+
+    if (wait_for_radiation) {
+      // This is a heuristic to avoid very small time steps at the beginning of the simulation when no stars have formed.
+      // loop over the cells and find the max value of e_rad_0 in the domain.
+      // If e_rad_0 < 1e-19, we consider that there is no radiation and we can use the hydro/particle cfl conditions instead of the radiative one.
+      UserData::FieldAccessor Urt_check = U.getAccessor({ {"e_rad_0", 0} });
+
+      std::vector<UserData::FieldAccessor::FieldInfo> rt_clean_fields;
+      rt_clean_fields.reserve(4 * n_groups);
+
+      for (int g = 0; g < n_groups; ++g) {
+        const int off = 4 * g;
+        rt_clean_fields.push_back({"e_rad_"  + std::to_string(g), off + 0});
+        rt_clean_fields.push_back({"fx_rad_" + std::to_string(g), off + 1});
+        rt_clean_fields.push_back({"fy_rad_" + std::to_string(g), off + 2});
+        rt_clean_fields.push_back({"fz_rad_" + std::to_string(g), off + 3});
+      }
+
+      UserData::FieldAccessor Urt_clean = U.getAccessor(rt_clean_fields);
+
+      real_t max_e_rad = 0;
+      foreach_cell.reduce_cell( "compute_dt_rad_max_e_rad", U.getShape(),
+      KOKKOS_LAMBDA( const ForeachCell::CellIndex& iCell, real_t& max_e_rad_update )
+      {
+        max_e_rad_update = FMAX( max_e_rad_update, Urt_check.at(iCell, 0) );
+      }, Kokkos::Max<real_t>(max_e_rad) );
+      if (max_e_rad < 1e-19) {
+        // set the radiation to 1e-20 (there are numerical errors which can build up)
+        foreach_cell.foreach_cell( "compute_dt_rad_set_e_rad", U.getShape(),
+        KOKKOS_LAMBDA( const ForeachCell::CellIndex& iCell )
+        {
+          for (int i = 0; i < 4*n_groups; i++)
+            Urt_clean.at(iCell, i) = (i%4 == 0) ? 1e-20 : 0;
+        } );
+        return;
+      }
+    }
 
     // TODO : We don't have to iterate on every cell for this
     real_t inv_dt;
@@ -58,6 +99,8 @@ public:
 private:
   ForeachCell& foreach_cell;
   real_t c_rad;
+  int n_groups;
+  bool wait_for_radiation;
   real_t cfl;
   
 };
