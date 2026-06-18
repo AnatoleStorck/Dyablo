d3c15fe606a5f800540cf0f43755cd5599c7774b
Anatole Storck <storckanatole@gmail.com>
2026-06-18 12:24:53 -0500

    Add HydroUpdate_euler_27_passive_scalars class and registration

--- core/src/hyperbolic/HydroUpdate_euler.cpp
+++ core/src/hyperbolic/HydroUpdate_euler.cpp
@@ -18,6 +18,7 @@ namespace {
   using HyperbolicPolicy_Hydro_7_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_Hydro_impl, 7> >;
   using HyperbolicPolicy_Hydro_16_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_Hydro_impl, 16> >;
   using HyperbolicPolicy_Hydro_25_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_Hydro_impl, 25> >;
+  using HyperbolicPolicy_Hydro_27_passive_scalars = HyperbolicPolicy_base< HyperbolicPolicy_passive_scalars_impl<HyperbolicPolicy_Hydro_impl, 27> >;
 }
 
 class HydroUpdate_euler_2_passive_scalars 
@@ -60,6 +61,13 @@ public:
   using Hyperbolic_euler<HyperbolicPolicy_Hydro_25_passive_scalars>::Hyperbolic_euler;
 };
 
+class HydroUpdate_euler_27_passive_scalars 
+  : public Hyperbolic_euler<HyperbolicPolicy_Hydro_27_passive_scalars>
+{
+public:
+  using Hyperbolic_euler<HyperbolicPolicy_Hydro_27_passive_scalars>::Hyperbolic_euler;
+};
+
 } //namespace dyablo
 
 FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory, 
@@ -85,3 +93,7 @@ FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory,
 FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory, 
                   dyablo::HydroUpdate_euler_25_passive_scalars, 
                   "HydroUpdate_euler_25_passive_scalars")
+
+FACTORY_REGISTER( dyablo::HyperbolicUpdateFactory, 
+                  dyablo::HydroUpdate_euler_27_passive_scalars, 
+                  "HydroUpdate_euler_27_passive_scalars")
